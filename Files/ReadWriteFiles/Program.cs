8cf1f93f72480c49f541def559b942dae20d5e6d
Neccentric <obiscondu@gmail.com>
2025-02-13 10:21:07 +0100

    Read-Write File

--- Files/ReadWriteFiles/Program.cs
+++ Files/ReadWriteFiles/Program.cs
@@ -2,18 +2,25 @@
 // Reading and Writing data from and to files
 
 // Make sure the example file exists
-const string filename = "TestFile.txt";
+const string filename = "TextFile.txt";
 
 // TODO 1: WriteAllText overwrites a file with the given content
 if (!File.Exists(filename)) {
-
+    File.WriteAllText(filename, "This is Application Programming");
 }
 
 // TODO 3: AppendAllText adds text to an existing file
-
+File.AppendAllText(filename, " This is a 300 level second semester course ");
 
 // TODO 4: A FileStream can be opened and written to until closed
-
+using (StreamWriter sw = File.AppendText(filename)) {
+    sw.WriteLine(" Ndu");
+    sw.WriteLine(" Emmanuel");
+    sw.WriteLine("BU/22C/IT/7739 ");
+}
 
 // TODO 2: ReadAllText reads all the content from a file
+string content;
+content = File.ReadAllText(filename);
+Console.WriteLine(content);
 
