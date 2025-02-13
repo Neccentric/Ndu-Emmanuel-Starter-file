73c1aaaa605e2e821df16fd748d6574526863195
Neccentric <obiscondu@gmail.com>
2025-02-13 10:07:15 +0100

    Creating and Renaming Files

--- Files/CreateFiles/Program.cs
+++ Files/CreateFiles/Program.cs
@@ -1,10 +1,20 @@
 ﻿// Application Programming .NET Programming with C# by Abdullahi Tijjani
 // Creating and Deleting Files
 
-const string filename = "TestFile.txt";
+const string ClassFile = "TextFile.txt";
 
 // TODO: Create a new file - this will overwrite any existing file
 // Use the "using" construct to automatically close the file stream
-
-
-// TODO: Determine if a file exists
+using (StreamWriter sw = File.CreateText(ClassFile)) {
+    sw.WriteLine("This is a text file.");
+ }
+// // TODO: Determine if a file exists
+Console.WriteLine(File.Exists(ClassFile));
+if (File.Exists(ClassFile)) {
+    File.Delete(ClassFile);
+}else {
+    using (StreamWriter sw = File.CreateText(ClassFile)) {
+        sw.WriteLine("This is a text file.");
+    }
+}
+Console.WriteLine(File.Exists(ClassFile));
