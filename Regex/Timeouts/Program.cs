e1e388904ad8b16ca3d1d99b637db1a543d16c85
Neccentric <obiscondu@gmail.com>
2025-02-20 10:28:09 +0100

    TimeOut

--- Regex/Timeouts/Program.cs
+++ Regex/Timeouts/Program.cs
@@ -3,18 +3,19 @@
 using System.Text.RegularExpressions;
 using System.Diagnostics;
 
-const string thestr = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+const int MAX_REGEX_TIME = 1000; //Timeout value in milliseconds
+const string thestr = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
 
 // Use a Stopwatch to show elapsed time
 Stopwatch sw;
 
 // TODO: Use a Timeout value when executing RegEx to guard against bad input
-
+TimeSpan Timeout = TimeSpan.FromMilliseconds(MAX_REGEX_TIME);
 
 // Run the expression and output the result
 try {
     sw = Stopwatch.StartNew();
-    Regex CapWords = new Regex(@"(a+a+)+b", RegexOptions.None);
+    Regex CapWords = new Regex(@"(a+a+)+b", RegexOptions.None, Timeout);
     MatchCollection mc = CapWords.Matches(thestr);
     sw.Stop();
     Console.WriteLine($"Found {mc.Count} matches in {sw.Elapsed} time:");
@@ -22,6 +23,7 @@ try {
         Console.WriteLine($"'{match.Value}' found at position {match.Index}");
     }
 }
-catch (Exception e) {
-    Console.WriteLine(e);
+catch (RegexMatchTimeoutException e) {
+    Console.WriteLine($"The Regex took long to execute! {e.MatchTimeout}");
 }
+
